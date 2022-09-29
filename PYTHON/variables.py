55878bb0063deef20b6fae8f0c5404d5d963e0e6
Esdras-Hernanny <esdrashernannym@gmail.com>
2022-09-29 16:36:38 -0300

    Organizando os estudos por temas.

---
+++ PYTHON/variables.py
@@ -0,0 +1,20 @@
+# Início dos estudos em Python
+
+# Tema 1 - Sintaxe básica
+
+# 1.1 - Declarando variáveis
+
+# Python é uma linguagem de tipagem dinâmica logo, não é necessário
+# definir os tipos de dados ao declarar uma variável. Segue a declaração a seguir:
+
+x = 10 
+
+print(x) # Retorna 10 no console.
+
+# Nesse caso como temos a variável "x" armazenando um valor "n" de forma dinâmica
+# se atribuirmos um novo valor a variável e um novo tipo não teremos nenhum problema.
+# Exemplo:
+
+x = "dez"
+
+print(x) # Embora as duas funções "print()" estajam chamando a variável "x" termos duas saídas diferentes.
